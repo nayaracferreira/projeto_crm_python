c9f95936912fc71475db1eb2029515d6bf05b375
Nayara do Carmo Ferreira <nayara.dcf@gmail.com>
2022-11-07 15:58:35 -0300

    Registrando Blueprints

---
+++ routes/empresa_routes.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, request
+
+from config.database import cursor, connection
+
+empresa_bp = Blueprint("empresa", __name__)
+
+# CREATE
+@empresa_bp.route("/empresa", methods=["POST"])
+def create():
+    body = request.json    
+    insert_sql = """
+    INSERT INTO empresa (razao, cnpj, telefone, email, cep, endereco, numero, bloco, bairro, cidade, uf, senha) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """
+    insert_tuple = (body["razao"]), (body["cnpj"]), (body["telefone"]), (body["email"]), (body["cep"]), (body["endereco"]), (body["numero"]), (body["bloco"]), (body["bairro"]), (body["cidade"]), (body["uf"]), (body["senha"])
+    cursor.execute(insert_sql, insert_tuple)     
+    connection.commit()
+    return {}, 201
+
+# READ
+@empresa_bp.route("/empresa/all", methods=["GET"])
+def read_all():
+    select_sql = """
+    SELECT * FROM empresa 
+    """
+    cursor.execute(select_sql)
+    todas_empresas = cursor.fetchall()
+    return {"empresa": todas_empresas}, 200
+
+@empresa_bp.route("/empresa/<empresa_id>", methods=["GET"])
+def read(empresa_id):
+    select_sql = """
+    SELECT * FROM empresa WHERE id = %s
+    """
+    cursor.execute(select_sql, (empresa_id,))
+    empresa = cursor.fetchone()
+    return {"empresa": empresa}, 200
+
+#UPDATE
+@empresa_bp.route("/empresa/<empresa_id>", methods=["PUT"])
+def update(empresa_id):
+    body = request.json
+    update_sql = """
+    UPDATE empresa SET razao = %s, cnpj = %s, telefone = %s, email = %s, cep = %s, endereco = %s, numero = %s, bloco = %s, bairro = %s, cidade = %s, uf = %s, senha = %s WHERE id = %s
+    """
+    update_tuple = (body["razao"], body["cnpj"], body["telefone"], body["email"], body["cep"], body["endereco"], body["numero"], body["bloco"], body["bairro"], body["cidade"], body["uf"], body["senha"], empresa_id)
+    cursor.execute(update_sql, update_tuple)
+    connection.commit()
+    return {}, 200
+
+#DELETE
+@empresa_bp.route("/empresa/<empresa_id>", methods=["DELETE"])
+def delete(empresa_id):
+    delete_sql = """
+    DELETE FROM empresa WHERE id = %s
+    """   
+    cursor.execute(delete_sql, (empresa_id,))
+    connection.commit()
+    return {}, 204
+
