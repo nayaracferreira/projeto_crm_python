7b7169aec2d73b39063c229fad737bafd1b5fd73
Nayara do Carmo Ferreira <nayara.dcf@gmail.com>
2022-11-28 11:21:10 -0300

    Inclusão de cors

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
+
+#, cross_origin
+#Caso não ser certo utiliza o @cross_origin() abaixo dos @ , 
 
 from routes.empresa_routes import empresa_bp
 from routes.login_routes import login_bp
@@ -6,6 +10,8 @@ from routes.calendario_routes import calendario_bp
 from routes.agenda_routes import agenda_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(empresa_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(calendario_bp)
