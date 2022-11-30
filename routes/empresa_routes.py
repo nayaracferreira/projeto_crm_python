95bf5c06061179ed3aaad18ae5be5dc027a9c185
Nayara do Carmo Ferreira <nayara.dcf@gmail.com>
2022-11-29 21:27:37 -0300

    Atualização da rota  read empresa/all

--- routes/empresa_routes.py
+++ routes/empresa_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request,jsonify
 from config.database import cursor, connection
 import bcrypt
 
@@ -7,9 +7,9 @@ empresa_bp = Blueprint("empresa", __name__)
 # CREATE
 @empresa_bp.route("/empresa", methods=["POST"])
 def create():
-    body = request.get_json()    
+    body = request.json    
     insert_sql = """
-    INSERT INTO empresa (razao, cnpj, telefone, email, cep, endereco, numero, bloco, bairro, cidade, uf, senha) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);    """
+    INSERT INTO empresas (razao, cnpj, telefone, email, cep, endereco, numero, bloco, bairro, cidade, uf, senha) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);    """
     hashed = bcrypt.hashpw(body["senha"].encode("utf-8"), bcrypt.gensalt())
     insert_tuple = (body["razao"]), (body["cnpj"]), (body["telefone"]), (body["email"]), (body["cep"]), (body["endereco"]), (body["numero"]), (body["bloco"]), (body["bairro"]), (body["cidade"]), (body["uf"]), hashed.decode("utf-8")
     cursor.execute(insert_sql, insert_tuple,)     
@@ -31,7 +31,26 @@ def read_all():
     """
     cursor.execute(select_sql)
     todas_empresas = cursor.fetchall()
-    return {"empresa": todas_empresas}, 200
+    connection.commit()
+
+    lista_retornar_json = []
+    for x in todas_empresas:
+        informacao_dia_retornadas={}
+        informacao_dia_retornadas["id"]= x[0]
+        informacao_dia_retornadas["razao"]= x[1]
+        informacao_dia_retornadas["cnpj"]= x[2]
+        informacao_dia_retornadas["telefone"]= x[3]
+        informacao_dia_retornadas["email"]= x[4]
+        informacao_dia_retornadas["cep"]= x[5]
+        informacao_dia_retornadas["endereco"]= x[6]
+        informacao_dia_retornadas["numero"]= x[7]
+        informacao_dia_retornadas["bloco"]= x[8]
+        informacao_dia_retornadas["bairro"]= x[9]
+        informacao_dia_retornadas["cidade"]= x[10]
+        informacao_dia_retornadas["uf"]= x[11]
+        informacao_dia_retornadas["senha"]= x[12]
+        lista_retornar_json.append(informacao_dia_retornadas)
+    return jsonify(lista_retornar_json), 200
 
 @empresa_bp.route("/empresa/<empresa_id>", methods=["GET"])
 def read(empresa_id):
