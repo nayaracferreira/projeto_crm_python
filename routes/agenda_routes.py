1ab8aacd8586c40d1b996944a268bf0ba0775832
Nayara do Carmo Ferreira <nayara.dcf@gmail.com>
2022-12-04 12:41:28 -0300

    Alteração na rota /todomes

--- routes/agenda_routes.py
+++ routes/agenda_routes.py
@@ -38,8 +38,8 @@ def DeleteDia():
 
     return{},200
     
-@agenda_bp.route('/todomes',methods=["GET"])
-def AllMes():
+@agenda_bp.route('/todomes',methods=["POST"])
+def allMes():
     json_data = request.get_json()
 
     email_da_empresa = json_data['email_da_empresa']
@@ -58,6 +58,8 @@ def AllMes():
     for x in toda_informacao_email_especifico:
         toda_informacao_email_especifico={}
         toda_informacao_email_especifico["Dia"]= x[0]
+        toda_informacao_email_especifico["Email"]= x[1]
+        toda_informacao_email_especifico["Nota"]= x[2]
         # print(x)
         lista_retornar_json.append(toda_informacao_email_especifico)
     return jsonify(lista_retornar_json),200 
@@ -89,3 +91,49 @@ def AllDia():
         lista_retornar_json.append(informacao_dia_retornadas)
 
     return jsonify(lista_retornar_json),200
+
+# PARTE DE TESTE
+
+teste1 =4
+teste2='Ronaldo'
+teste3='ronaldo@email'
+teste4="1234"
+
+@agenda_bp.route('/teste')
+def testando(): 
+
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute ('DROP TABLE IF EXISTS empressa')
+
+    script_criar = '''CREATE TABLE IF NOT EXISTS empressa (
+                    id int PRIMARY KEY,
+                    nomeempressa varchar(360) NOT NULL,
+                    emailempressa varchar(360) NOT NULL,
+                    senha varchar(360) NOT NULL)'''
+
+    script_inserir ='INSERT INTO empressa (id, nomeEmpressa, emailempressa, senha) VALUES (%s,%s,%s,%s)'
+    script_valores = (teste1,teste2,teste3,"123456")
+
+    cursor.execute(script_criar)
+    cursor.execute(script_inserir,script_valores)
+
+
+    # cur.execute('SELECT *  FROM EMPRESSA WHERE ID = 4')
+    # for record in cur.fetchall():
+    #     print(record['emailempressa'],record['senha'])
+    
+    cursor.execute(f'SELECT *  FROM EMPRESSA WHERE ID = {teste1}') #Verificar email se existe no banco de dados
+    x= cursor.fetchone()
+    if x == None: 
+        print ("Vazio") #Voltar para pagina porque não encontrou algo ou algo está errado
+    else:               #SENÃO , vai para verificar se a senha recebida está correta 
+        if teste4 == x[3]: 
+            print("Tudo Correto") #Vai para proxima pagina 
+        else:
+            print("Senha Errada") #Volta a pagina anterior// mensagaem para tentar novamente
+        
+        
+    connection.commit()
+
+    return "Teste Completo"
+
